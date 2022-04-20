aeffe66b7e67069f1506d29be5c30d35d702753c
zisyang <33085566+zisyang@users.noreply.github.com>
2022-04-20 04:17:09 -0700

    Update config_employee.js

--- js/config_employee.js
+++ js/config_employee.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_fl75xBhb4', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1ueom4kcnj61dajefaervf0mfi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_3Syhkib6q', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '195qcdull048n3pf7s6tim5orj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
