ffcfc2347ce9f75006f39c43d9592595c8ec3abf
Allen Wu <yanshiun.wu@sjsu.edu>
2022-04-20 03:54:46 -0700

    Add and Update config_employee.js

---
+++ js/config_employee.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_fl75xBhb4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1ueom4kcnj61dajefaervf0mfi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
