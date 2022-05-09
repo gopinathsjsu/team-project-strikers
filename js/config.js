f5c8521ef5a170f0cedab650ad6f81b7a87cea1f
zisyang <zishun.yang@sjsu.edu>
2022-05-08 23:47:12 -0700

    update booking api & test file

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://v69x1qvvyj.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://v69x1qvvyj.execute-api.us-west-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
